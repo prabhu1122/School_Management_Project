8014d3a5fc78ca1532b6643ad0d7ca4de25a2649
prabhu1122 <prabhat199541@gmail.com>
2023-02-19 22:36:06 +0530

    added charts to hod home

--- static/assets/plugins/apexchart/chart-data.js
+++ static/assets/plugins/apexchart/chart-data.js
@@ -5,37 +5,38 @@ $(document).ready(function() {
 	// Area chart
 	
 	if ($('#apexcharts-area').length > 0) {
-	var options = {
-		chart: {
-			height: 350,
-			type: "area",
-			toolbar: {
-				show: false
-			},
-		},
-		dataLabels: {
-			enabled: false
-		},
-		stroke: {
-			curve: "smooth"
-		},
-		series: [{
-			name: "Teachers",
-			data: [45, 60, 75, 51, 42, 42, 30]
-		}, {
-			name: "Students",
-			color: '#FFBC53',
-			data: [24, 48, 56, 32, 34, 52, 25]
-		}],
-		xaxis: {
-			categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-		}
-	}
-	var chart = new ApexCharts(
-		document.querySelector("#apexcharts-area"),
-		options
-	);
-	chart.render();
+        var options = {
+            chart: {
+                height: 350,
+                type: "area",
+                toolbar: {
+                    show: false
+                },
+            },
+            dataLabels: {
+                enabled: true
+            },
+            stroke: {
+                curve: "smooth"
+            },
+            series: [{
+                name: "Teachers",
+                color: '#00AA53',
+                data: [45, 60, 75, 51, 42, 42, 30]
+            }, {
+                name: "Students",
+                color: '#FFBC53',
+                data: [24, 48, 56, 32, 34, 52, 25]
+            }],
+            xaxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+            }
+        }
+        var chart = new ApexCharts(
+            document.querySelector("#apexcharts-area"),
+            options
+        );
+        chart.render();
 	}
 
 	// Bar chart
