54ec1fc2240d4e88d34575458aeac436d529c3ca
prabhu1122 <prabhat199541@gmail.com>
2023-03-04 11:11:26 +0530

    reset the feedback portion

--- myApp/models.py
+++ myApp/models.py
@@ -108,4 +108,4 @@ class StaffFeedback(models.Model):
   updated_at = models.DateTimeField(auto_now=True)
   
   def __str__(self):
-    return self.staff_id.admin.first_name + "" + self.staff_id.admin.last_name
+    return self.staff_id.admin.first_name + " " + self.staff_id.admin.last_name
